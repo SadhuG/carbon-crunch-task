8275411ac8bfcb83721e1bcea69b79918e1733b5
Sudhansh <96883913+SadhuG@users.noreply.github.com>
2025-03-21 13:13:38 +0530

    hero skeleton (No styling)

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,75 @@
+// image imports
+import logo from "./assets/carbon-crunch-logo.png";
+import handIcon from "./assets/hand-icon.png";
+
+// ui imports
+import { Button } from "./components/ui/button";
+
 const App = () => {
+  const navMenu = ["Service", "Blog", "About us", "Contact"];
   return (
     <div>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <header>
+        <nav>
+          {/* logo */}
+          <div>
+            <img src={logo} alt="Carbon Crunch Logo" />
+          </div>
+
+          {/* nav items */}
+          <div>
+            {/* nav menu */}
+            <ul>
+              {navMenu.map((item, index) => (
+                <a href="#" key={index}>
+                  <li>{item}</li>
+                </a>
+              ))}
+            </ul>
+
+            <div>
+              <Button>Login</Button>
+              <Button>Book Demo</Button>
+            </div>
+          </div>
+        </nav>
+
+        {/* hero */}
+        <section>
+          {/* tagline */}
+          <div>
+            <img src={handIcon} alt="Hand Waving Icon" />
+            <p>Let’s Save our Environment</p>
+          </div>
+
+          {/* main page heading */}
+          <h1>
+            Streamline Your Sustainability Reporting With{" "}
+            <span>CARBON CRUNCH</span>
+          </h1>
+
+          {/* sub-heading */}
+          <div>
+            <p>
+              <span>95%</span> Accurate Carbon Calculations Trusted by Industry
+              Leaders
+            </p>
+          </div>
+
+          {/* CTA */}
+          <div>
+            <Button>Free Calculator</Button>
+            <Button>Book Demo</Button>
+          </div>
+        </section>
+      </header>
+
+      {/* slider section */}
+      <main>
+        <section>
+          <div></div>
+        </section>
+      </main>
     </div>
   );
 };
